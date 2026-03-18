026628de0772786617bad22a10eb16c5b8ba1529
SergioAgGames <sergioaguado164@gmail.com>
2026-03-18 10:26:01 +0100

    Cambiamos textos

--- Assets/Scripts/AnswerController.cs
+++ Assets/Scripts/AnswerController.cs
@@ -2,17 +2,13 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 using UnityEngine.UI;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine.EventSystems;
-
-
 
 public class AnswerController : MonoBehaviour
 {
     [SerializeField] private GameObject AnswerPanel;
     [SerializeField] private TMP_Text dialogueText;
-    [SerializeField] private int  answerText;
-    
+    [SerializeField] private int answerText;
 
     [SerializeField] private InputField inputText;
 
@@ -21,35 +17,49 @@ public class AnswerController : MonoBehaviour
     public bool correctText = false;
 
     [SerializeField] private TextController textController;
-    
+
     private void Update()
     {
+        // Se ejecuta todo el rato para comprobar si escribes lo correcto
         TextValidate();
     }
 
     public void ActivateAnswerPanel(int index)
     {
-     
+        // Si llegamos a la línea de la pregunta...
         if (index == answerText)
         {
-             
             AnswerPanel.SetActive(true);
+
+            // Si no ha acertado, mantenemos el botón apagado
+            if (correctText == false)
+            {
+                textController.continueButton.interactable = false;
+                Debug.Log("Botón deshabilitado porque apareció la pregunta");
+            }
+            else
+            {
+                // Por si acaso ya la había acertado, lo encendemos
+                textController.continueButton.interactable = true;
+            }
         }
         else
         {
-           
             AnswerPanel.SetActive(false);
+
+            // NUEVO: Si es una línea de diálogo normal, VOLVEMOS A ENCENDER el botón 
+            // para que el jugador pueda seguir leyendo.
+            textController.continueButton.interactable = true;
         }
     }
     public void TextValidate()
     {
-        if (inputText.text == playerAnswer)
+        // Si el texto del Input coincide con la respuesta correcta
+        if (inputText.text == playerAnswer && correctText == false)
         {
             correctText = true;
-            Debug.Log("TExto Correcto");
-            textController.continueButton.interactable = true;
+            Debug.Log("Texto Correcto");
+            textController.continueButton.interactable = true; // Volvemos a encender el botón
         }
     }
-
-
 }
--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
-using UnityEngine.UI; 
-using Microsoft.Unity.VisualStudio.Editor;
-using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; // Quité Microsoft.Unity.VisualStudio.Editor que no es necesario aquí
 
 public class TextController : MonoBehaviour
 {
-    [SerializeField, TextArea (4,6)] private string[] dialogueLines;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
     [SerializeField] private string[] characterName;
     [SerializeField] private Sprite[] characterSprite;
-     
 
     [SerializeField] private GameObject textPanel;
     [SerializeField] private GameObject namePanel;
@@ -21,22 +19,24 @@ public class TextController : MonoBehaviour
     [SerializeField] private UnityEngine.UI.Image characterImage;
     public Button continueButton;
 
+    // NUEVO: Este es tu interruptor mágico. Si está en false, ignorará el AnswerController.
+    [SerializeField] private bool tienePreguntas = false;
 
     [SerializeField] private Collider2D statueCollider;
 
     private int lineIndex;
     private int characternamesIndex;
     private int spriteIndex;
-    float typingTime = 0.05f;
+    float typingTime = 0.02f;
 
     private bool textStarted = false;
-
-
+    private bool showLine = false;
     [SerializeField] private AnswerController answerController;
+
     public void StartDialogue()
     {
         textStarted = true;
-   
+
         textPanel.SetActive(true);
         lineIndex = 0;
         characternamesIndex = 0;
@@ -44,60 +44,62 @@ public class TextController : MonoBehaviour
 
         statueCollider.enabled = false;
         StartCoroutine(ShowLine());
-    
-        
-  
-    }
-    private IEnumerator ShowLine()
-    {
-        dialogueText.text = string.Empty;
-        foreach(char ch in dialogueLines[lineIndex])
-        {
-            dialogueText.text += ch;
-            yield return new WaitForSeconds(typingTime);
-        }
     }
 
     public void nextLine()
     {
+        // NUEVO: Apagamos el botón nada más pulsarlo para evitar el "doble clic"
+        continueButton.interactable = false;
+
         lineIndex++;
-      
         spriteIndex++;
 
-       
-        Debug.Log(lineIndex.ToString());
+        Debug.Log("Línea actual: " + lineIndex.ToString());
 
         if (lineIndex < dialogueLines.Length)
         {
-
             StartCoroutine(ShowLine());
-            answerController.ActivateAnswerPanel(lineIndex);
-
-            if (answerController.correctText == false)
-            {
-
-                continueButton.interactable = false;
-                Debug.Log("Boton deshabilitado");
-
-            }
-          
+            // Hemos quitado la llamada a ActivateAnswerPanel de aquí. 
+            // Ahora se llamará cuando termine de escribir el texto.
         }
         else
         {
-           
             textPanel.SetActive(false);
-
             statueCollider.enabled = true;
 
             // Diálogo terminado  marcar esta escena como completada
             string currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
             GameController.Instance?.CompleteScene(currentScene);
+        }
+    }
 
+    private IEnumerator ShowLine()
+    {
+        // NUEVO: Aseguramos que el botón esté apagado mientras las letras aparecen
+        continueButton.interactable = false;
+
+        dialogueText.text = string.Empty;
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }
+
+        // NUEVO: El texto ha terminado de escribirse.
+        if (tienePreguntas && answerController != null)
+        {
+            // Le pasamos el control al AnswerController. 
+            // Él decidirá si apaga el botón (si es pregunta) o lo enciende (si es diálogo normal).
+            answerController.ActivateAnswerPanel(lineIndex);
+        }
+        else
+        {
+            // Si la estatua NO tiene preguntas, simplemente encendemos el botón para que lea la siguiente línea.
+            continueButton.interactable = true;
         }
     }
     public void UpdateCharacterInfo()
     {
-     
         if (characterName.Length > lineIndex)
         {
             nameText.text = characterName[lineIndex];
@@ -108,6 +110,4 @@ public class TextController : MonoBehaviour
             characterImage.sprite = characterSprite[lineIndex];
         }
     }
-
-
 }
