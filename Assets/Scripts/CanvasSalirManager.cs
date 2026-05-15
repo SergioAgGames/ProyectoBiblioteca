2dbba303129ebe3485317d5ddc47ed52f4339ef2
SergioAgGames <sergioaguado164@gmail.com>
2026-05-15 11:16:15 +0200

    Menu salir

---
+++ Assets/Scripts/CanvasSalirManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class CanvasSalirManager : MonoBehaviour
+{
+    
+    public GameObject pantallaCanvas;
+
+    void Update()
+    {
+
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            pantallaCanvas.SetActive(pantallaCanvas);
+
+        }
+    }
+    public void CerrarJuego()
+    {
+
+        Application.Quit();
+
+    }
+    public void OcultarCanvas()
+    {
+        if (pantallaCanvas != null)
+            pantallaCanvas.SetActive(false);
+    }
+}
